c6d7234dfdf84580ead8bf89bf86873e9d98372c
shesha0987654321 <shesha6040@gmail.com>
2024-11-22 17:28:28 +0530

    new header component

---
+++ src/app/main-header/main-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-header',
+  standalone: false,
+  
+  templateUrl: './main-header.component.html',
+  styleUrl: './main-header.component.css'
+})
+export class MainHeaderComponent {
+
+}
